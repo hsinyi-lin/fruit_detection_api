f03b3f38299e5348b7278d24268ac423d5c6693c
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-11-10 15:10:20 +0800

    feat: add db models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    email = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=45, blank=True, null=True)
+    nickname = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'account'
+
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=45, blank=True, null=True)
+    content = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'fruit'
+
+
+class Recipe(models.Model):
+    email = models.CharField(max_length=45, blank=True, null=True)
+    title = models.CharField(max_length=45, blank=True, null=True)
+    step = models.CharField(max_length=45, blank=True, null=True)
+    ingredient = models.CharField(max_length=45, blank=True, null=True)
+    picture = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'recipe'
